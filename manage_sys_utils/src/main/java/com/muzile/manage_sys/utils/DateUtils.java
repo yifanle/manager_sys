4e89e90c80fcf197a7aafd921c654a72b5f44ddf
muzile <49034243+yifanle@users.noreply.github.com>
2021-02-04 16:18:06 +0800

    Add files via upload

---
+++ manage_sys_utils/src/main/java/com/muzile/manage_sys/utils/DateUtils.java
@@ -0,0 +1,22 @@
+package com.muzile.manage_sys.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtils {
+
+    //日期转换成字符串
+    public static String date2String(Date date, String patt) {
+        SimpleDateFormat sdf = new SimpleDateFormat(patt);
+        String format = sdf.format(date);
+        return format;
+    }
+
+    //字符串转换成日期
+    public static Date string2Date(String str, String patt) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat(patt);
+        Date parse = sdf.parse(str);
+        return parse;
+    }
+}
---
+++ manage_sys_utils/src/main/java/com/muzile/manage_sys/utils/MailUtils.java
@@ -0,0 +1,52 @@
+package com.muzile.manage_sys.utils;
+
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
+
+public class MailUtils {
+
+	public static String sendMail(String msg,String email)
+			throws Exception {
+		// 1.创建一个程序与邮件服务器会话对象 Session
+
+		Properties props = new Properties();
+		props.setProperty("mail.transport.protocol", "SMTP");
+		props.setProperty("mail.host", "smtp.126.com");
+		props.setProperty("mail.smtp.auth", "true");// 指定验证为true
+
+		// 创建验证器
+		Authenticator auth = new Authenticator() {
+			public PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("muzile_593", "HYQIJUNKMZCMRDDD");
+			}
+		};
+
+		Session session = Session.getInstance(props, auth);
+
+		// 2.创建一个Message，它相当于是邮件内容
+		Message message = new MimeMessage(session);
+
+		message.setFrom(new InternetAddress("muzile_593@126.com")); // 设置发送者
+
+		message.setRecipient(RecipientType.TO, new InternetAddress(email)); // 设置发送方式与接收者
+
+		message.setSubject("库存预警");
+
+		message.setContent(msg, "text/html;charset=utf-8");
+
+		// 3.创建 Transport用于将邮件发送
+
+		Transport.send(message);
+
+		return "200";
+	}
+}
